6ad96ef0eb050fd69145087f33a778df5595e32f
dilusha07 <dilushasandaruwani07@gmail.com>
2021-09-11 21:10:34 +0530

    Refactor Footer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,10 +6,12 @@ height: 5vh;
 
 export const Main = styled.main`
 min-height: 100vh;
-padding: 2.5vh
+padding: 2.5vh;
 `;
 
 export const Footer = styled.footer`
 display: flex;
 justify-content: center;
+padding: 1em;
+border-top: 1px solid #ccc;
 `;
