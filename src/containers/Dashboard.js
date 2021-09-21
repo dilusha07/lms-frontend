6de779fb1c8f5ec9a03004c17e80c254f9b6ad3b
dilusha07 <dilushasandaruwani07@gmail.com>
2021-09-22 00:22:25 +0530

    Test axios and inttegrate getBooks API

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
-import Spinner from "../components/Spinner";
 import Tabs from "../components/Tabs";
- 
+
+import { getBooks } from "../api/bookAPI";
+import Spinner from "../components/Spinner";
+
+
 const Dashboard = () => {
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+      setIsLoading(true);
+      getBooks()
+        .then((Response) => {
+            if(!Response.error) {
+                console.log(Response.data);
+            }
+        })
+        .catch((error)=> {
+            console.log(error);
+        })
+        .finally(() => {
+            setIsLoading(false);
+        })
+        
+    }, [])
     const contents = [
         {
             title: "Books", 
@@ -18,9 +39,7 @@ const Dashboard = () => {
      
     return (
        
-        <Tabs contents ={contents}/>
-        
-       
+        isLoading ? <Spinner /> : <Tabs contents ={contents}/>   
     );
 };
 
