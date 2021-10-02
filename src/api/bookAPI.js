23f054c3ca354fdc91c22d72a6b583159b829324
dilusha07 <dilushasandaruwani07@gmail.com>
2021-10-02 13:02:36 +0530

    Add getBook API

--- src/api/bookAPI.js
+++ src/api/bookAPI.js
@@ -2,4 +2,6 @@ import { getRequest } from "./util";
 
 const BASE_URL = "/book";
 
-export const getBooks = () => getRequest('${BASE_URL}');
+export const getBooks = () => getRequest(`${BASE_URL}`);
+
+export const getbook = (id) => getRequest(`${BASE_URL}/${id}`);
