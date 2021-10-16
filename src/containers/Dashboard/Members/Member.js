a6b04e8698e62df317a14ac2cb539e94d57b84b6
dilusha07 <dilushasandaruwani07@gmail.com>
2021-10-16 22:44:35 +0530

    Implement Delete Member

--- src/containers/Dashboard/Members/Member.js
+++ src/containers/Dashboard/Members/Member.js
@@ -2,12 +2,23 @@ import React, {useState, useEffect} from "react";
 import {IoReturnUpBack} from "react-icons/io5";
 import styled from "styled-components";
 
+import {
+    Container,
+    ContainerInline,
+    FlexRow,
+    Button,
+} from "../../../components/CommonComponents";
 
-import {getMember} from "../../api/memberAPI"
+import Spinner from "../../components/Spinner";
+import ConfirmationDialog from "../../../components/ConfirmationDialog";
+
+import {getMember, deleteMember} from "../../api/memberAPI"
+import { getTodaysDate } from "../../../shared/utils";
 
 const Member = ({id, handleBackClick}) => {
     const [isLoading, setIsLoading] = useState(false);
     const [member, setMember] = useState(null);
+    const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
 
     useEffect(() => {
         setIsLoading(true);
@@ -25,16 +36,24 @@ const Member = ({id, handleBackClick}) => {
             });
         },[id]);
 
+        const handleDelete = (confirmed) => {
+            if (confirmed) {
+                deleteMember(member.id);
+            }
+            setShowDeleteConfirmation(false);
+        };
+
    return(
+       <>
        <Container>
            <Button onClick={handleBackClick} size = {1.5}>
                 <IoReturnUpBack />
             </Button>
             {!isLoading && member !== null ? (
-                    
+                <>    
                         <FlexRow>
                             <ContainerInline>
-                                <H1>{member.name}</H1>
+                                <H1>{member.fname}</H1>
                                 <H2>{member.phoneNo}</H2>
                                 <p>
                                     Lorem ipsum dolor sit amet, consectetur
@@ -45,10 +64,29 @@ const Member = ({id, handleBackClick}) => {
                             </ContainerInline>
                             
                             </FlexRow>
+                            <FlexRow>
+                              <Button
+                                        color="danger"
+                                        onClick={() =>
+                                            setShowDeleteConfirmation(true)
+                                        }
+                                    >
+                                        Delete
+                                    </Button>
+                               
+                            </FlexRow>
+                           </>
                             ):(
                                 <Spinner />
                             )}
        </Container>
+       <ConfirmationDialog
+       handleClose={handleDelete}
+       show={showDeleteConfirmation}
+       headerText="Confirm deletion"
+       detailText="Are you sure you want to delete this member? This action can't be undone."
+   />
+   </>
    );
 
 };
